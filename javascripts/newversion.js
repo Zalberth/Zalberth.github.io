ac760736b2ae60fb8edc9541572eb2a4d43f922b
albertzmh <albertzmh@sina.com>
2015-06-14 01:48:38 +0800

    with ajax, a new version homepage, some clean to be done

---
+++ javascripts/newversion.js
@@ -0,0 +1,42 @@
+$(function() {
+        
+       
+
+      var rightsecWidth = parseInt($('#mainbody').css('width'))-parseInt($('#leftsec').css('width')) - 10;
+        $('#right_container').css('width' , rightsecWidth + 'px');
+      
+        $(window).resize(function() {
+          rightsecWidth = parseInt($('#mainbody').css('width'))-parseInt($('#leftsec').css('width')) - 10;
+          $('#right_container').css('width' , rightsecWidth-1 + 'px'); //minus 1 can fix a bug
+        });
+
+        $('#zmh_links').on('click',function(e) {
+          e.preventDefault();
+          window.open("https://github.com/Zalberth");    
+        });
+
+       	$.get('articles/resume.html', function(data) {
+        	$('#contentDisplayer').html(data);
+        });
+  
+        $('#article_list').on('click',function(e) {
+        	e.preventDefault();
+        	var _target = $(e.target).data('target');  //get the value data-*
+        	$.get('articles/'+_target+'.html', function(data) {
+		       $('#contentDisplayer').html(data);
+		    });
+        	//Bind click event to ul
+        });
+        
+        
+
+        $('#tools').on('click',function(e) {
+        	 e = e || window.event;  
+    		if(e.stopPropagation) { //W3C阻止冒泡方法  
+      		  e.stopPropagation();  
+    		} else {  
+      		  e.cancelBubble = true; //IE阻止冒泡方法  
+    		}  
+    		
+        });
+      });
