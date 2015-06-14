4b1eb564a8a03ad1bdbe44c141862f4ffdf4d37c
albertzmh <albertzmh@sina.com>
2015-06-14 22:40:38 +0800

    re

--- javascripts/newversion.js
+++ javascripts/newversion.js
@@ -1,46 +1,31 @@
 $(function() {
-        
+             
       var mq = window.matchMedia( "(max-device-width: 800px)" );
       if(mq.matches) {
       	$('#article_list').css('display','none');
+      	$('#articleLists').on('tap',function(e) {
+      		$(this).animate({height:'100px'});
+      	});
+      }else {
+      	      	            	              	             	        
       }
-       
+              $('#zmh_links').on('click',function(e) {
+                e.preventDefault();
+                window.open("https://github.com/Zalberth");    
+              });
 
-      var rightsecWidth = parseInt($('#mainbody').css('width'))-parseInt($('#leftsec').css('width')) - 10;
-        $('#right_container').css('width' , rightsecWidth + 'px');
+             	$.get('articles/resume.html', function(data) {
+              	$('#contentDisplayer').html(data);
+              });
+        
+              $('#article_list').on('click',function(e) {
+              	e.preventDefault();
+              	var _target = $(e.target).data('target');  //get the value data-*
+              	$.get('articles/'+_target+'.html', function(data) {
+      		       $('#contentDisplayer').html(data);
+      		    });
+              	//Bind click event to ul
+              });  
+
       
-        $(window).resize(function() {
-          rightsecWidth = parseInt($('#mainbody').css('width'))-parseInt($('#leftsec').css('width')) - 10;
-          $('#right_container').css('width' , rightsecWidth-1 + 'px'); //minus 1 can fix a bug
-        });
-
-        $('#zmh_links').on('click',function(e) {
-          e.preventDefault();
-          window.open("https://github.com/Zalberth");    
-        });
-
-       	$.get('articles/resume.html', function(data) {
-        	$('#contentDisplayer').html(data);
-        });
-  
-        $('#article_list').on('click',function(e) {
-        	e.preventDefault();
-        	var _target = $(e.target).data('target');  //get the value data-*
-        	$.get('articles/'+_target+'.html', function(data) {
-		       $('#contentDisplayer').html(data);
-		    });
-        	//Bind click event to ul
-        });
-        
-        
-
-        $('#tools').on('click',function(e) {
-        	 e = e || window.event;  
-    		if(e.stopPropagation) { //W3C阻止冒泡方法  
-      		  e.stopPropagation();  
-    		} else {  
-      		  e.cancelBubble = true; //IE阻止冒泡方法  
-    		}  
-    		
-        });
       });
