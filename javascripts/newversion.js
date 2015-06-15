8b63918139d061fa775b4d934a88442c64f4697b
ZHAOMUHUA <albertzmh@sina.com>
2015-06-15 09:46:27 +0800

    in xjtc

--- javascripts/newversion.js
+++ javascripts/newversion.js
@@ -1,35 +1,41 @@
 $(function() {
              
-      var mq = window.matchMedia( "(max-device-width: 800px)" );
+      var mq = window.matchMedia( "(max-device-width: 1600px)" );
       var listsClicked = 0;
       var listNum = 0;
       var listItemsHeight = 0;
-      if(mq.matches) {
-      	$('#article_list').css('display','none');
-      	$('#toggleArticleLists').on('click',function(e) {
-      		if(listsClicked === 0){
-      			
-      			listNum = $('#article_list').children().length;
-      			listItemHeight = listNum*parseInt($('.zmh_cursor').css('height')) + 50;
-      		
-      			$('#articleLists').animate({height:listItemHeight+'px'},200,function() {
-      				$('#toggleArticleLists').text('SHRINK');
-      			});
-      			$('#article_list').css('display','block');
-      			listsClicked = 1;
-      		}else {
-      			$('#articleLists').animate({height:40+'px'},200,function() {
-      				$('#article_list').css('display','none');
-      				$('#toggleArticleLists').text('EXPAND');
-      			});
-      			
-      			listsClicked = 0;
-      		}
-      		
-      	});
-      }else {
-      	      	            	              	             	        
+      var isMobile = 0
+      mediaSizeChange(mq); //for First-time Load 
+      mq.addListener(mediaSizeChange);
+      function mediaSizeChange(mediaQuery) { //Listening to the Media Query Change
+        // body...
+        if(mediaQuery.matches && isMobile === 0){
+            $('#article_list').css('display','none');
+            $('#toggleArticleLists').on('click',function(e) {
+              if(listsClicked === 0){
+                
+                listNum = $('#article_list').children().length;
+                listItemHeight = listNum*parseInt($('.zmh_cursor').css('height')) + 50;
+              
+                $('#articleLists').animate({height:listItemHeight+'px'},200,function() {
+                  $('#toggleArticleLists').text('SHRINK');
+                });
+                $('#article_list').css('display','block');
+                listsClicked = 1;
+              }else {
+                $('#articleLists').animate({height:40+'px'},200,function() {
+                  $('#article_list').css('display','none');
+                  $('#toggleArticleLists').text('EXPAND');
+                });
+                
+                listsClicked = 0;
+              }             
+            });
+            isMobile = 1;
+
+        }
       }
+
               $('#zmh_links').on('click',function(e) {
                 e.preventDefault();
                 window.open("https://github.com/Zalberth");    
