cdd893f5d5c9bb694e503ebd2395a61ad8a7c755
albertzmh <albertzmh@sina.com>
2015-08-13 23:36:42 +0800

    re

--- javascripts/lab/dote.js
+++ javascripts/lab/dote.js
@@ -4,8 +4,8 @@
 		this.each(function() { //may have multiple jquery objs
 		var positionState = $(this).css('position');
 			if( positionState != 'static') {
-					if(positionState === 'fixed'){
-							$(this).on('click',function(e) {
+					if( positionState === 'fixed' ){
+							$(this).on('click', function(e) {
 								e.preventDefault();
 								var 
 								x, y;
@@ -25,9 +25,9 @@
 						         $('.circle').animate({top:y- 5 + 'px',left:x - 5 + 'px',width:'20px',height:'20px',borderRadius:'10px',opacity:settings.finalOpacity,backgroundColor:settings.finalBkColor},settings.duration,function() {
 						    	     $(this).remove();
 						         });
-							});
-						}else if(positionState === 'absolute') {
-								$(this).on('click',function(e) {
+							}); 
+						}else if( positionState === 'absolute' ) {
+								$(this).on('click', function(e) {
 									e.preventDefault();
 									var 
 									x, y;
@@ -43,7 +43,7 @@
 							         $(this).append(marked);
 			
 							       
-							         $('.circle').animate({top:y- 5 + 'px',left:x - 5 + 'px',width:'20px',height:'20px',borderRadius:'10px',opacity:settings.finalOpacity,backgroundColor:settings.finalBkColor},settings.duration,function() {
+							         $('.circle').animate( {top:y- 5 + 'px', left:x - 5 + 'px', width:'20px', height:'20px', borderRadius:'10px', opacity:settings.finalOpacity, backgroundColor:settings.finalBkColor}, settings.duration, function() {
 							    	     $(this).remove();
 							         });
 								});
