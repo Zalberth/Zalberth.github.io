3c7ea666817ecc24c99a90ce74e5125b25e7c988
albertzmh <albertzmh@sina.com>
2015-10-27 23:29:02 +0800

    maker

--- lab/js/maker/reservation.js
+++ lab/js/maker/reservation.js
@@ -4,9 +4,30 @@ $(function(){
 	//在list中插入未来的七天
 	var nextdayt = today.getTime();
 	var nextday = new Date(nextdayt);
-	var lists = '';
+	var lists = '', day = '';
+
 	for( var i = 0; i< 12; i++ ) {
-		lists += '<li>' + (nextday.getMonth()+1)+'-'+ nextday.getDate() + '</li>';
+
+		switch(nextday.getDay()) {
+			case 0: day = '星期天'; break;
+			case 1: day = '星期一'; break;
+			case 2: day = '星期二'; break;
+			case 3: day = '星期三'; break;
+			case 4: day = '星期四'; break;
+			case 5: day = '星期五'; break;
+			case 6: day = '星期六'; break;
+		}
+		lists += '<li>' +
+		         '<div class="item">' + 
+		         	'<div class="date">' 
+		         	+ (nextday.getMonth()+1) +'-'+ nextday.getDate() 
+		         	+'</div>' 
+		         	+ '<div class="weekday">'
+		         	+ day 
+		         	+ '</div>' + 
+		          '</div>'		
+		         +'</li>';
+
 		nextdayt = nextdayt + 1000*60*60*24;
 	    nextday = new Date(nextdayt);
 	} 
